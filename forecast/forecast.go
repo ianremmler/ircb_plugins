cf0e31ac65a75a5c788fe96f49f38295a4f8e22b
Ian Remmler <ian@remmler.org>
2013-04-25 05:48:14 -0500

    Initial commit.

---
+++ forecast/forecast.go
@@ -0,0 +1,134 @@
+package forecast
+
+import (
+	"code.google.com/p/go-charset/charset"
+	"github.com/jteeuwen/go-pkg-xmlx"
+
+	"fmt"
+	"go/build"
+	"math"
+	"time"
+)
+
+const (
+	fcUrlFmt    = "http://forecast.weather.gov/MapClick.php?lat=%s&lon=%s&FcstType=digitalDWML"
+	arrows      = "↑↗→↘↓↙←↖"
+	firstOctile = 0x2581
+	maxHours    = 48
+)
+
+func forecast(loc location) string {
+	charset.CharsetDir = build.Default.GOPATH + "/src/code.google.com/p/go-charset/datafiles"
+	doc := xmlx.New()
+	url := fmt.Sprintf(fcUrlFmt, loc.Lat, loc.Lon)
+	err := doc.LoadUri(url, charset.NewReader)
+	if err != nil {
+		return ""
+	}
+
+	startTimeNodes := doc.SelectNodes("", "start-valid-time")
+	endTimeNodes := doc.SelectNodes("", "end-valid-time")
+	if len(startTimeNodes) == 0 || len(endTimeNodes) == 0 {
+		return ""
+	}
+	if len(endTimeNodes) > maxHours {
+		endTimeNodes = endTimeNodes[:maxHours]
+	}
+	startTime, _ := time.Parse(time.RFC3339, startTimeNodes[0].Value)
+	endTime, _ := time.Parse(time.RFC3339, endTimeNodes[len(endTimeNodes)-1].Value)
+
+	temps := findVals("temperature", "hourly", doc)
+	humids := findVals("humidity", "", doc)
+	precips := findVals("probability-of-precipitation", "", doc)
+	speeds := findVals("wind-speed", "sustained", doc)
+	dirs := findVals("direction", "", doc)
+
+	minTemp, maxTemp, tempGraph := makeGraph(temps)
+	minHumid, maxHumid, humidGraph := makeGraph(humids)
+	minPrecip, maxPrecip, precipGraph := makeGraph(precips)
+	minSpeed, maxSpeed, speedGraph := makeGraph(speeds)
+
+	dirGraph := ""
+	for _, dir := range dirs {
+		idx := dirIndex(dir)
+		dirGraph += string([]rune(arrows)[idx])
+	}
+
+	timeFmt := "2006-01-02 15:04"
+	start, end := startTime.Format(timeFmt), endTime.Format(timeFmt)
+
+	tempRange := fmt.Sprintf("%3d %3d", minTemp, maxTemp)
+	humidRange := fmt.Sprintf("%3d %3d", minHumid, maxHumid)
+	precipRange := fmt.Sprintf("%3d %3d", minPrecip, maxPrecip)
+	speedRange := fmt.Sprintf("%3d %3d", minSpeed, maxSpeed)
+
+	out := fmt.Sprintf("Forecast for %s\n", loc.Name)
+	out += fmt.Sprintf("         min max %-24s%24s\n", start, end)
+	out += fmt.Sprintf("Temp °F  %7s %s\n", tempRange, tempGraph)
+	out += fmt.Sprintf("Humid %%%%  %7s %s\n", humidRange, humidGraph) // dbl esc % for later fmt use
+	out += fmt.Sprintf("Precip %%%% %7s %s\n", precipRange, precipGraph)
+	out += fmt.Sprintf("Wind mph %7s %s\n", speedRange, speedGraph)
+	out += fmt.Sprintf("Wind dir         %s\n", dirGraph)
+
+	return out
+}
+
+func minmax(vals []int) (int, int) {
+	min, max := math.MaxInt32, math.MinInt32
+	for _, v := range vals {
+		if v < min {
+			min = v
+		}
+		if v > max {
+			max = v
+		}
+	}
+	return min, max
+}
+
+func rescale(val, min, max, bins int) int {
+	if min >= max {
+		return 0
+	}
+	v := (val - min) * bins / (max - min)
+	if v < 0 {
+		v = 0
+	} else if v > bins-1 {
+		v = bins - 1
+	}
+	return v
+}
+
+func dirIndex(dir int) int {
+	return ((dir + 360/16) * 8 / 360) % 8
+}
+
+func findVals(name, typ string, doc *xmlx.Document) []int {
+	vals := []int{}
+	nodes := doc.SelectNodes("", name)
+	for _, node := range nodes {
+		if typ == "" || node.As("", "type") == typ {
+			for _, kid := range node.Children {
+				vals = append(vals, kid.I("", "value"))
+				if len(vals) >= maxHours {
+					break
+				}
+			}
+			break // just use the first set
+		}
+	}
+	return vals
+}
+
+func makeGraph(vals []int) (int, int, string) {
+	if len(vals) == 0 {
+		return 0, 0, ""
+	}
+	graph := ""
+	min, max := minmax(vals)
+	for _, val := range vals {
+		octile := rescale(val, min, max, 8)
+		graph += string(firstOctile + octile)
+	}
+	return min, max, graph
+}
---
+++ forecast/plugin.go
@@ -0,0 +1,86 @@
+package forecast
+
+import (
+	"github.com/jteeuwen/ircb/cmd"
+	"github.com/jteeuwen/ircb/plugin"
+	"github.com/jteeuwen/ircb/proto"
+
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"regexp"
+	"strings"
+)
+
+const (
+	locUrlFmt = "http://nominatim.openstreetmap.org/search?format=json&limit=1&q=%s"
+)
+
+type Plugin struct {
+	*plugin.Base
+}
+
+type location struct {
+	Name string `json:"display_name"`
+	Lat  string `json:"lat"`
+	Lon  string `json:"lon"`
+}
+
+func init() { plugin.Register(New) }
+
+func New(profile string) plugin.Plugin {
+	plug := &Plugin{}
+	plug.Base = plugin.New(profile, "forecast")
+	return plug
+}
+
+func (p *Plugin) Load(client *proto.Client) error {
+	err := p.Base.Load(client)
+	if err != nil {
+		return err
+	}
+	cmnd := &cmd.Command{
+		Name:        "forecast",
+		Description: "Print a pretty forecast for the given location",
+		Execute:     forecastCmd,
+	}
+	cmnd.Params = []cmd.Param{{
+		Name:        "loc",
+		Description: "Location to get forecast for",
+		Pattern:     cmd.RegAny,
+	}}
+	cmd.Register(cmnd)
+
+	return nil
+}
+
+func forecastCmd(cmd *cmd.Command, client *proto.Client, msg *proto.Message) {
+	loc := regexp.MustCompile("\\s+").ReplaceAllLiteralString(cmd.Data, "+")
+	targ := msg.Receiver
+	if !msg.FromChannel() {
+		targ = msg.SenderName
+	}
+	const locErr = "I had a problem finding the location."
+
+	resp, err := http.Get(fmt.Sprintf(locUrlFmt, loc))
+	if err != nil {
+		client.PrivMsg(targ, locErr)
+		return
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		client.PrivMsg(targ, locErr)
+		return
+	}
+	dec := json.NewDecoder(resp.Body)
+	locs := []location{}
+	dec.Decode(&locs)
+	if len(locs) == 0 {
+		client.PrivMsg(targ, locErr)
+		return
+	}
+	fc := forecast(locs[0])
+	for _, line := range strings.Split(fc, "\n") {
+		client.PrivMsg(targ, line)
+	}
+}
